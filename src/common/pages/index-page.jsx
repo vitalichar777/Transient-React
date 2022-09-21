3a9e6b7d9e6ce6a1282d8f2fefa155ecf0e1dc4e
vitalichar777 <w@wellit.ca>
2022-09-21 09:43:31 -0400

    equipment page status update

--- src/common/pages/index-page.jsx
+++ src/common/pages/index-page.jsx
@@ -149,8 +149,41 @@ export default class IndexPage extends React.Component {
             }
           }
 
+          if (cell.id === 'eventJobNumber') {
+            if (cell.value && cell.value.includes('Reserved')) {
+              flag = 1;
+            }
+          }
+
           return result
         });
+
+        if ( flag == 1 ) {
+          cells = row.cells.map(cell => {
+            const result = { ...cell }
+            if (cell.type === 'button') {
+              if (cell.callback === 'details') {
+                result.callback = this.handleDetailsClick
+              } else if (cell.callback === 'showEquipment') {
+                result.callback = this.handleShowEquipmentClick
+              } else if (cell.callback === 'showModels') {
+                result.callback = this.handleShowModelsClick
+              } else if (cell.callback === 'edit') {
+                result.callback = this.handleEditClick
+              } else if (cell.callback === 'delete') {
+                result.callback = this.handleDeleteClick
+              }
+            }
+
+            if (cell.id === 'eventStatus') {
+              if (cell.value && cell.value.includes('OUT')) {
+                result.value = 'RESERVED'
+              }
+            }
+  
+            return result
+          })
+        }
 
         return { id: row.id, cells }
       })
